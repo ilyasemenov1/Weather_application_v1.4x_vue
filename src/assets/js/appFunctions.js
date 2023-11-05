35e4a40949fe4beb5447c00c10f892f5dd32668f
Ilya Semenov <sialogacc@gmail.com>
2023-11-06 00:15:48 +0300

    Add app js functions

---
+++ src/assets/js/appFunctions.js
@@ -0,0 +1,150 @@
+export const constructDate = () => {
+
+    const settings = JSON.parse(localStorage.getItem("document-settings"));
+
+    let options = {
+        hour12: false,
+        hour: '2-digit',
+        minute:'2-digit'
+    }
+
+    if (!isNaN(settings)) {
+        return;
+    }
+
+    if (settings["time-format"] == "12h") {
+        options.hour12 = true;
+    }
+
+    const date = new Date();
+    return date.toLocaleDateString('ru-RU', options).split(", ")[1];
+}
+
+export const conventDtTxt = (dt) => {
+    let hours = dt.split(" ")[1];
+    let time = hours.split(":");
+    return `${time[0]}:${time[1]}`;
+}
+
+export const setIcon = (index) => {
+    return `./icons/weather_icons/${index}.svg`;
+}
+
+export const transformPressureToSettingUnit = (pressure) => {
+    const settings = JSON.parse(localStorage.getItem("document-settings"));
+    const unit = settings["units"]["pressure"];
+
+    switch (unit) {
+        case "mm-rt":
+            return Math.round(pressure * 0.75);
+        case "hpa":
+            return pressure;
+        case "atm":
+            return Math.round(pressure * 0.75 / 760.002 * 10**5) / 10**5;
+    }
+}
+
+export const transformSpeedToSettingUnit = (speed) => {
+    const settings = JSON.parse(localStorage.getItem("document-settings"));
+    const unit = settings["units"]["speed"];
+
+    switch (unit) {
+        case "mps":
+            return Math.round(speed);
+        case "kmph":
+            return Math.round(speed * 3.6);
+        case "milph":
+            return Math.round(speed * 2.23694);
+    }
+}
+
+export const transformTempToSettingUnit = (temp_k) => {
+    let temp = 0;
+    const settings = JSON.parse(localStorage.getItem("document-settings"));
+    const unit = settings["units"]["temp"];
+
+    switch (unit) {
+        case "c":
+            temp = Math.round(temp_k - 273.15);
+            break;
+        case "f":
+            temp = Math.round((temp_k - 273.15) * 9/5 + 32);
+            break;
+        case "k":
+            temp = Math.round(temp_k);
+    }
+
+    let tempRet = 0;
+
+    if (temp > 0) {
+        tempRet = `+${temp}`;
+    } else {
+        tempRet = temp;
+    }
+
+    return tempRet;
+}
+
+export const arrayMin = (arr) => {
+    return arr.reduce((p, v) => {
+        return ( p < v ? p : v );
+    });
+}
+
+export const arrayMax = (arr) => {
+    return arr.reduce((p, v) => {
+        return ( p > v ? p : v );
+    });
+}
+
+export const dtConventer = (dt, isUtc) => {
+
+    const settings = JSON.parse(localStorage.getItem("document-settings"));
+
+    let options = {
+        hour12: false,
+        hour: '2-digit',
+        minute:'2-digit'
+    }
+
+    if (!isNaN(settings)) {
+        return;
+    }
+
+    if (settings["time-format"] == "12h") {
+        options.hour12 = true;
+    }
+
+    if (isUtc) {
+        let date = new Date((dt - 180*60)*1000);
+        return date.toLocaleDateString('ru-RU', options).split(", ")[1];
+    } else {
+        let date = new Date(dt*1000);
+        return date.toLocaleDateString('ru-RU', options).split(", ")[1];
+    }
+
+}
+
+export const getDate = (index) => {
+    const date = new Date();
+    date.setDate(date.getDate() + index + 1);
+
+    if (index != 0) {
+        var options = {
+            weekday: 'long',
+            month: 'long', 
+            day: 'numeric' 
+        };
+
+        return date.toLocaleDateString('ru-RU', options);
+    } else {
+        var options = {
+            month: 'long', 
+            day: 'numeric' 
+        };
+
+        return `завтра, ${date.toLocaleDateString('ru-RU', options)}`;
+    }
+}
+
+export const average = (array) => array.reduce((a, b) => a + b) / array.length;
---
+++ src/assets/js/weatherInfo.js
@@ -0,0 +1,83 @@
+
+export async function getWeather(place, key) {
+    return await fetch(`https://api.openweathermap.org/data/2.5/forecast/?q=${place}&appid=${key}&lang=ru&cnt=40`)
+}
+
+function success(pos) {
+    const crd = pos.coords;
+
+    getCity(crd.latitude, crd.longitude);
+}
+
+function error() {
+    // generate_err_notification("geolocation");
+    console.log("error")
+}
+
+function getUserLocation() {
+    const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+}
+
+function getCity(lat, lng) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', `https://us1.locationiq.com/v1/reverse.php?key=${token}&lat=${lat}&lon=${lng}&format=json`, true);
+    xhr.send();
+    xhr.addEventListener("readystatechange", () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var response = JSON.parse(xhr.responseText);
+            let place = "";
+    
+            if (response.address.town) {
+                place = response.address.town;
+            } else {
+                place = response.address.city;
+            }
+
+            return place;
+     
+            // const settings = JSON.parse(localStorage.getItem("document-settings"));
+    
+            // if (settings["data-update"] != "disable") {
+            //     const weather_interval = setInterval(() => {
+            //     }, transferTimeFromSettingsToMs());
+    
+            //     sessionStorage.setItem("weather-interval", JSON.stringify(weather_interval));
+            // }
+        }
+    }, false);
+}
+
+function transferTimeFromSettingsToMs() {
+    const settings = JSON.parse(localStorage.getItem("document-settings"));
+
+    if (!isNaN(settings)) {
+        throw new Error("Settings not found");
+    }
+
+    let settings_data = settings["data-update"];
+    let time_type = settings_data.match(/s|min|h/)[0];
+    if (time_type) {
+        let time_count = settings_data.split(time_type)[0];
+
+        switch (time_type) {
+            case "s":
+                return +time_count * 1000;
+            case "min":
+                return +time_count * 1000 * 60;
+            case "h":
+                return +time_count * 1000 * 3600;
+            default:
+                throw new Error("Another time type or no time type");
+        }
+    } else {
+        throw new Error("Another time type or no time type");
+    }
+}   
+
