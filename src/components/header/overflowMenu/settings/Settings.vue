c5cbdc6278bd61d7257c97f5844da6b3702e636d
Ilya Semenov <sialogacc@gmail.com>
2023-11-06 03:17:55 +0300

    Add settings components

--- src/components/header/overflowMenu/settings/Settings.vue
+++ src/components/header/overflowMenu/settings/Settings.vue
@@ -1,10 +1,28 @@
 <script setup>
     import rootLevel2MenuComponent from '../rootLevel2MenuComponent.vue';
+    import ThemeMenuSelect from './ThemeMenuSelect.vue';
+    import SettingsSwitch from './SettingsSwitch.vue';
+    import SettingsSelect from './SettingsSelect.vue';
 </script>
 
 <template>
     <rootLevel2MenuComponent id="settings">
         <template #labelText>Настройки</template>
-        <template #content></template>
+        <template #content>
+            <h2>Интерфейс:</h2>
+            <ThemeMenuSelect />
+            <SettingsSwitch>
+                Фиксировать шапку сайта при прокручивании
+            </SettingsSwitch>
+            <SettingsSwitch>
+                Упростить анимацию открытия меню
+            </SettingsSwitch>
+            <SettingsSwitch>
+                Показывать избранные города при поиске
+            </SettingsSwitch>
+            <SettingsSelect :options='{"12h": "12-часовой", "24h": "24-часовой"}'>
+                Временной формат:
+            </SettingsSelect>
+        </template>
     </rootLevel2MenuComponent>
 </template>
---
+++ src/components/header/overflowMenu/settings/ThemeMenuSelect.vue
@@ -0,0 +1,106 @@
+<script setup>
+    import { onMounted, ref } from "vue";
+    
+    let selectMode = ref("auto");
+    let markerWidth = ref(0);
+    let markerLeft = ref(0);
+
+    function setMarkerPosition() {
+        const formInputs = document.querySelectorAll(".dark-mode-form input");
+        const form = document.querySelector(".dark-mode-form");
+
+        let selected = null;
+        formInputs.forEach(element => {
+            if (element.checked) selected = element;
+        })
+
+        
+        let parent = selected.parentElement;
+        let parentWidth = parent.clientWidth;
+        let parentRect = parent.getBoundingClientRect();
+        let parentWindowLeft = parentRect.left;
+        let formWindowLeft = form.getBoundingClientRect().left;
+        
+        markerLeft.value = parentWindowLeft - formWindowLeft;
+        markerWidth.value = parentWidth;
+    }
+
+    onMounted(() => {
+        setMarkerPosition();
+    });
+</script>
+
+<template>
+    <form class="dark-mode-form">
+        <span class="dark-mode-form__button-name">Тема:</span>
+        <div class="dark-mode-form__theme-select" @click="setMarkerPosition">
+            <div class="dark-mode-form__select-container">
+                <input type="radio" id="night-mode-active" class="dark-mode-form__button-input" name="dark-mode" value="active" v-model="selectMode">
+                <label for="night-mode-active">Тёмная</label>
+            </div>
+            <div class="dark-mode-form__select-container">
+                <input type="radio" id="night-mode-auto" class="dark-mode-form__button-input" name="dark-mode" value="auto" v-model="selectMode">
+                <label for="night-mode-auto">Авто</label>
+            </div>
+            <div class="dark-mode-form__select-container">
+                <input type="radio" id="night-mode-disactive" class="dark-mode-form__button-input" name="dark-mode" value="disactive" v-model="selectMode">
+                <label for="night-mode-disactive">Светлая</label>
+            </div>
+        </div>
+        <div class="marker" :style="{ width: `${markerWidth}px`, transform: `translateX(${markerLeft}px)`}"></div>
+    </form>
+</template>
+
+<style scoped>
+    .dark-mode-form__button-name {
+        padding: 0 0 0 10px;
+        font-size: 17px;
+        font-weight: 500;
+        color: var(--text-color-1);
+    }
+    .dark-mode-form {
+        position: relative;
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        font-family: "Sourse Sans Pro", sans-serif;
+    }
+    .dark-mode-form label {
+        font-size: 17px;
+        padding: 3px 7px;
+        border-radius: 20px;
+        font-weight: 500;
+        color: var(--text-color-1);
+        transition: .3s ease;
+    }
+    .dark-mode-form label:hover {
+        cursor: pointer;
+    }
+    .dark-mode-form__button-input {
+        position: absolute;
+        opacity: 0;
+        pointer-events: none;
+    }
+    .dark-mode-form__theme-select {
+        display: flex;
+        gap: 10px;
+        padding: 8px 5px;
+        border-radius: 30px;
+        outline: #00000000 solid;
+        background: #a3a3a32b;
+    }
+    .dark-mode-form__select-container {
+        z-index: 11;
+    }
+    .marker {
+        position: absolute;
+        top: 4px;
+        left: 0;
+        height: 30px;
+        background: var(--bg-color-2);
+        border-radius: 50px;
+        box-shadow: 0 0 5px #0000001e;
+        transition: .2s ease;
+        z-index: 10;
+    }
+</style>
