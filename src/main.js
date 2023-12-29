c2119580376db0653ab716b339467223f2b1d150
Ilya Semenov <sialogacc@gmail.com>
2023-12-29 14:24:44 +0300

    Add vite pwa support, add editorconfig & remove old  dependencies

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import { createPinia } from 'pinia'
-import './registerServiceWorker.js'
 
 const pinia = createPinia()
 const app = createApp(App)
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,39 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 import { defineConfig } from 'vite'
-import vueJsx from '@vitejs/plugin-vue-jsx'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      includeAssets: ['logo.svg', './img/icons/favicon-32x32.png'],
+      manifest: {
+        name: 'Погода',
+        short_name: 'Погода',
+        description: 'Приложение погоды, способное отображать прогноз в любой точке мира!',
+        theme_color: '#4a4c4e',
+        lang: "ru",
+        icons: [
+          {
+            src: 'logo.svg',
+            sizes: 'any'
+          },
+          {
+            src: './img/icons/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
